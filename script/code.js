66dae1bf319cf3a0651f02e7b54eb8cea9cf464d
Ziaaaaaaaaaaaa <ziakhan29042003@gmail.com>
2023-05-24 15:50:52 +0200

    commit

---
+++ script/code.js
@@ -0,0 +1,39 @@
+// variable
+// a name of storage location.
+
+// value
+// The value property sets or returns the value of the value attribute of a text field.
+
+// Data type
+// In Javascript, there are five basic, or primitive, types of data. The five most basic types of data are strings, numbers, booleans, undefined, and null
+
+// initialization
+// Storing a value in a variable
+
+// difference between let var and const 
+
+// let allows you to declare variables that are limited to the scope of a block statement, or expression on which it is used.
+
+// var statement declares a function-scoped or globally-scoped variable, optionally initializing it to a value.
+
+// const keyword specifies that a variable's value is constant and tells the compiler to prevent the programmer from modifying it.
+
+// what is a variable scope 
+// The scope of a variable is the block of code in the entire program where the variable is declared, used, and can be modified.
+
+// When can you decide when to use a for or while, forEach and do while loop?
+
+// A for Loop is used to repeat a specific block of code a number of times that is known.
+// A while Loop is used to repeat a specific block of code an unknown number of times until the conditions are met.
+// A foreach loop is used when you need to operate on each item individually in an array.
+// A do while loop is used in all scenarios where the loop body needs to be executed at least once.
+
+// Define hoisting and variable scoping?
+
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+// varible scope
+// refers to the current context of code, which determines the accessibility of variables to JavaScript.
+
+
+let fullName = ("Zia Khan")
+console.log(fullName);
